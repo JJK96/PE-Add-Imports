0c58c805cf7445afd27526f090b826d424eac94d
Jan-Jaap Korpershoek <jjkorpershoek96@gmail.com>
2023-12-27 14:48:57 +0100

    Take input from arguments

--- Test.cpp
+++ Test.cpp
@@ -2,15 +2,13 @@
 
 int main(int argc, char* argv[]) {
 	// Open the input file
-	PEFile pe("1.exe");
+	PEFile pe(argv[0]);
 
-	// Add "MessageBoxA" & "ShowWindow" functions to the import table
-	char* functions[] = { "MessageBoxA", "ShowWindow" };
-	pe.addImport("user32.dll", functions, 2);
+	pe.addImport(argv[1], &argv[3], 1);
 	
 	// Add a new section named ".at4re" with size "0x1000" byte
 	pe.addSection(".at4re", 0x1000, false);
 	
 	// Save the modified file
-	pe.saveToFile("1+.exe");
+	pe.saveToFile(argv[2]);
 }
